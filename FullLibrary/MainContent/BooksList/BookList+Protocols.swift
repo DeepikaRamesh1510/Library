b694a85cb4fa0d20029b858ecb3a4deee81ff3d2
user <user@FULL232-AIR.FULL.Creative>
2019-12-16 12:34:16 +0530

    The searchBar delegate methods are mooved to a new helper file

--- FullLibrary/MainContent/BooksList/BookList+Protocols.swift
+++ FullLibrary/MainContent/BooksList/BookList+Protocols.swift
@@ -66,10 +66,6 @@ extension BooksListViewController: UITableViewDelegate, UITableViewDataSource, U
 //		}
 	
 	
-	
-	// MARK: TableViewDelegate methods
-	
-	
 	// MARK: BookProtocol conformation
 	
 	//	func performAction(flowState: FlowState,book newBook : Book) {
@@ -79,59 +75,5 @@ extension BooksListViewController: UITableViewDelegate, UITableViewDataSource, U
 	//		books.append(newBook)
 	//		tableView.reloadData()
 	//	}
-	
-	// MARK: search bar operations
-	
-	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-		guard searchText.length > 0  else {
-			return
-		}
-		if libraryState == .goodReads {
-			fetchBooksInGoodReadsLibrary(bySearch: self.searchText)
-		} else {
-			fetchBooksInMyLibrary(bySearch: searchText)
-		}
-		
-		self.searchBar.endEditing(true)
-	}
-	
-	func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-		self.searchText = searchText
-		if libraryState == .myLibrary {
-			fetchBooksInMyLibrary(bySearch: searchText)
-		}
-		
-	}
-	
-	//MARK: fetching books list from the goodsReads server
-	func fetchBooksInGoodReadsLibrary(bySearch searchString: String) {
-		GoodReadsNetworkRequest.shared.fetchBooks(bySearch: searchString) { (data,error) in
-			if let error = error {
-				print(error.localizedDescription)
-				return
-			}
-			guard let data = data else {
-				print("Data not received!")
-				return
-			}
-			let xmlResponseParser = XMLResponseParser()
-			self.goodReadsBooks = xmlResponseParser.parseTheXMLData(xmlData: data)
-			self.tableView.reloadData()
-		}
-	}
-	
-	//MARK: fetching books from my library
-	
-	func fetchBooksInMyLibrary(bySearch: String) {
-		if searchText.isEmpty {
-			guard let allBooks = ManageBooks.shared.fetchBooks() else {
-				return
-			}
-			self.myLibraryBooks = allBooks
-		} else {
-			self.myLibraryBooks = ManageBooks.shared.fetchBooksBasedOnSearch(by: searchText)
-		}
-		self.tableView.reloadData()
-	}
-	
+
 }
