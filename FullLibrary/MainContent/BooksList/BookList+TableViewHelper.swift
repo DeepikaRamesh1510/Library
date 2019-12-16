1efe46f726d7774ccb9a1577814d0896c82ae6e9
user <user@FULL232-AIR.FULL.Creative>
2019-12-16 12:11:25 +0530

    Checking the book count and displaying appropriate message or count

--- FullLibrary/MainContent/BooksList/BookList+TableViewHelper.swift
+++ FullLibrary/MainContent/BooksList/BookList+TableViewHelper.swift
@@ -20,69 +20,73 @@ extension BooksListViewController {
 		return bookCell
 	}
 	
+	func checkBookCount(rowCount: Int) {
+		if rowCount == 0 {
+			var message = String()
+			if self.searchText.length == 0 {
+				message = libraryState == .myLibrary ? "No Books added to the library" : "Search for books in Good Reads library"
+			} else {
+				message = "The searched book is not available in the library!"
+			}
+			self.tableView.setEmptyMessage(message)
+		} else {
+			self.tableView.restore()
+		}
+	}
+	
 	//MARK: table view delegate methods
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-			let rowCount: Int = libraryState == .myLibrary ? myLibraryBooks.count : goodReadsBooks.count
-					if rowCount == 0 {
-						var message = String()
-						if self.searchText.length == 0 {
-							message = libraryState == .myLibrary ? "No Books added to the library" : "Search for books in Good Reads library"
-						} else {
-							message = "The searched book is not available in the library!"
-						}
-						self.tableView.setEmptyMessage(message)
-					} else {
-						self.tableView.restore()
-					}
-			return rowCount
-		}
 		
-		func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-			guard libraryState == .goodReads  else {
-				 return modifyBookCellByState(title: myLibraryBooks[indexPath.item].title ?? "", author: myLibraryBooks[indexPath.item].authorName ?? "")
-			}
-			return modifyBookCellByState(title: goodReadsBooks[indexPath.item].title, author: goodReadsBooks[indexPath.item].authorName)
+		let rowCount: Int = libraryState == .myLibrary ? myLibraryBooks.count : goodReadsBooks.count
+		checkBookCount(rowCount: rowCount)
+		return rowCount
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		guard libraryState == .goodReads  else {
+			return modifyBookCellByState(title: myLibraryBooks[indexPath.item].title ?? "", author: myLibraryBooks[indexPath.item].authorName ?? "")
 		}
-		
-		func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-			if libraryState == .myLibrary {
-				if editingStyle == .delete {
-					var isDeleted: Bool = true
-					guard let bookISBN = myLibraryBooks[indexPath.item].isbn else {
-						showToast(message: "Deletion Failed!")
-						return
-					}
-					ManageBooks.shared.deleteBook(isbn: bookISBN) { (error) in
-						self.showToast(message: error.errorType.rawValue)
-						isDeleted = false
-						return
-					}
-					guard isDeleted else {
-						return
-					}
-					self.myLibraryBooks.remove(at: indexPath.row)
-					tableView.deleteRows(at: [indexPath], with: .fade)
-					print(indexPath.item)
-					
-				}
-			}
-			
-		}
-		
-		func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-			if libraryState == .myLibrary {
-				guard let bookDetailViewController = storyboard?.instantiateViewController(withIdentifier: "BookDetailViewController") as? BookDetailViewController else {
-					print("Failed to open instantiate book detail view controller")
+		return modifyBookCellByState(title: goodReadsBooks[indexPath.item].title, author: goodReadsBooks[indexPath.item].authorName)
+	}
+	
+	func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+		if libraryState == .myLibrary {
+			if editingStyle == .delete {
+				var isDeleted: Bool = true
+				guard let bookISBN = myLibraryBooks[indexPath.item].isbn else {
+					showToast(message: "Deletion Failed!")
 					return
 				}
-				bookDetailViewController.book = myLibraryBooks[indexPath.item]
-	//			bookDetailViewController.bookUpdationDelegate = self
-				navigationController?.pushViewController(bookDetailViewController, animated: true)
+				ManageBooks.shared.deleteBook(isbn: bookISBN) { (error) in
+					self.showToast(message: error.errorType.rawValue)
+					isDeleted = false
+					return
+				}
+				guard isDeleted else {
+					return
+				}
+				self.myLibraryBooks.remove(at: indexPath.row)
+				tableView.deleteRows(at: [indexPath], with: .fade)
+				print(indexPath.item)
+				
 			}
-			
-			tableView.deselectRow(at: indexPath, animated: true)
 		}
 		
+	}
+	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		if libraryState == .myLibrary {
+			guard let bookDetailViewController = storyboard?.instantiateViewController(withIdentifier: "BookDetailViewController") as? BookDetailViewController else {
+				print("Failed to open instantiate book detail view controller")
+				return
+			}
+			bookDetailViewController.book = myLibraryBooks[indexPath.item]
+			//			bookDetailViewController.bookUpdationDelegate = self
+			navigationController?.pushViewController(bookDetailViewController, animated: true)
+		}
+		
+		tableView.deselectRow(at: indexPath, animated: true)
+	}
+	
 }
